f774cf9d221b00a04c8e99a0ca5d71e0a5554afd
jeongdung-eo <jeuniis2@gmail.com>
2022-11-18 22:18:19 +0900

    [FEAT] Color

---
+++ 31st_Joint_Seminar/Resources/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  31st_Joint_Seminar
+//
+//  Created by JEONGEUN KIM on 2022/11/18.
+//
+//
+//import UIKit
+//struct Color {
+//    static let kybo_black : UIColor? = R.color.kyobo.kyobo_Black()
+//    static let kybo_dark_gray : UIColor? = R.color.kyobo.kybo_Dark_gray()
+//    static let kybo_gray : UIColor? = R.color.kyobo.kybo_Gray()
+//    static let kybo_light_gray : UIColor? = R.color.kyobo.kybo_Light_gray()
+//    static let kybo_green : UIColor? = R.color.kyobo.kybo_Green()
+//    static let kybo_light_green : UIColor? = R.color.kyobo.kybo_Light_green()
+//    static let kybo_blue : UIColor? = R.color.kyobo.kybo_Blue()
+//    static let kybo_red : UIColor? = R.color.kyobo.kybo_Red()
+//}
---
+++ 31st_Joint_Seminar/Resources/Font.swift
@@ -0,0 +1,8 @@
+//
+//  Font.swift
+//  31st_Joint_Seminar
+//
+//  Created by JEONGEUN KIM on 2022/11/18.
+//
+
+import Foundation
