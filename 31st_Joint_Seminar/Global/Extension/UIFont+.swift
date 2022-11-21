27a3e466aa0a9c7cf9c0c664d245e37d942c0369
daminoworld <ekadls757@gmail.com>
2022-11-21 19:46:43 +0900

    [Add] font+ 추가

--- 31st_Joint_Seminar/Global/Extension/UIFont+.swift
+++ 31st_Joint_Seminar/Global/Extension/UIFont+.swift
@@ -9,4 +9,19 @@ import UIKit
 
 extension UIFont {
     
+    class var kyoboIosH1: UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Bold", size: 18.0)!
+      }
+
+      class var kyoboIosH2: UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Regular", size: 18.0)!
+      }
+
+      class var kyoboIosH3: UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Bold", size: 16.0)!
+      }
+
+      class var kyoboIosBody1: UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Medium", size: 16.0)!
+      }
 }
