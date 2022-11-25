ddee9e9bc19b29eb7d29f41878897466e903e213
minseo205 <minseo770@gmail.com>
2022-11-25 22:36:26 +0900

    [CHORE] 책 이름과 작가 이름 요소 간 간격 조정

--- 31st_Joint_Seminar/Sources/Scenes/VCs/LibrarySearchCell/RecentModel.swift
+++ 31st_Joint_Seminar/Sources/Scenes/VCs/LibrarySearchCell/RecentModel.swift
@@ -15,8 +15,8 @@ struct RecentModel : Hashable {
 extension RecentModel {
     static var recentList : [RecentModel] = [
         RecentModel(bookImage: "LibrarySearch/bookImage1", title: "프로그래머를 위한 강화 학습", author: "멀티코어"),
-        RecentModel(bookImage: "LibrarySearch/bookImage2", title: "오직 두 사람", author: "김영하"),
-        RecentModel(bookImage: "LibrarySearch/bookImage3", title: "데미안", author: "헤르만 헤세"),
+        RecentModel(bookImage: "LibrarySearch/bookImage2", title: "오직 두 사람\n", author: "김영하"),
+        RecentModel(bookImage: "LibrarySearch/bookImage3", title: "데미안\n", author: "헤르만 헤세"),
         RecentModel(bookImage: "LibrarySearch/bookImage4", title: "지적 대화를 위한 넓고 얕은 지식", author: "채사장")
     ]
  }
